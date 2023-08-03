b99a52e2e47856ad7721549b88f8c8985e39f969
Yevhenii <eygehavv27@gmail.com>
2023-08-03 23:57:33 +0300

    feat: ui changes, app component changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
-import React from 'react';
-import './App.css';
-import TableComponent from './components/Table';
-import TableInfo from './interfaces/TableInfo';
-import TableButton from './interfaces/TableButton';
-import { ButtonAction } from './enums/ButtonAction';
+import './App.scss';
+import HomePage from './pages/HomePage';
+
 function App() {
 
-  const tableInfo: TableInfo = {
-    tHeads: ['head1', 'head2', 'head3'],
-    tRows: [
-      ['1', 'veryveryveryveryveryveryveryveryveryveryveryvery long text', 'img', [{
-        action: ButtonAction.EditNote,
-        attachedNoteId: "1111"
-      }],
-      [{
-        action: ButtonAction.ArchiveNote,
-        attachedNoteId: "1111"
-      }]],
-      ['2', 'veryveryveryveryveryveryveryveryveryveryveryvery long text', 'img', [{
-        action: ButtonAction.EditNote,
-        attachedNoteId: "1111"
-      }]],
-      ['3', 'veryveryveryveryveryveryveryveryveryveryveryvery long text', 'img', [{
-        action: ButtonAction.EditNote,
-        attachedNoteId: "1111"
-      }]],
-      ['41', 'veryveryveryveryveryveryveryveryveryveryveryvery long text', 'img', [{
-        action: ButtonAction.EditNote,
-        attachedNoteId: "1111"
-      }]],
-      ['5', 'veryveryveryveryveryveryveryveryveryveryveryvery long text', 'img', [{
-        action: ButtonAction.EditNote,
-        attachedNoteId: "1111"
-      }]],
-      ['6', 'veryveryveryveryveryveryveryveryveryveryveryvery long text', 'img', [{
-        action: ButtonAction.EditNote,
-        attachedNoteId: "1111"
-      }]]
-    ]
-  }
+  
   return (
     <div className="App">
       <main>
-        <TableComponent tHeads={tableInfo.tHeads} tRows={tableInfo.tRows} />
+        <HomePage></HomePage>
       </main>
     </div>
   );
